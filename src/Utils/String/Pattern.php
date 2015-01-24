a4e6c7ca01dc480012f7ea23555dc5aaf5edb259
Zoltán Borsos <zolli07@gmail.com>
2015-01-24 13:56:28 +0100

    *Started working on pattern matching

---
+++ src/Utils/String/Pattern.php
@@ -0,0 +1,18 @@
+<?php namespace buildr\Utils\String;
+
+/**
+ * BuildR - PHP based continuous integration server
+ *
+ * Basic pattern matching class
+ *
+ * @author Zoltán Borsos <zolli07@gmail.com>
+ * @package buildr
+ * @subpackage Utils\String
+ *
+ * @copyright    Copyright 2015, Zoltán Borsos.
+ * @license      https://github.com/Zolli/BuildR/blob/master/LICENSE.md
+ * @link         https://github.com/Zolli/BuildR
+ */
+class Pattern {
+
+}
